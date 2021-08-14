4f9f2b87f214af1f83ef5fe437a97c79e34a204a
aamuzakii <aamuzakii@gmail.com>
2021-08-14 17:02:25 +0700

    setup spring server

---
+++ src/main/java/com/bezkoder/spring/datajpa/model/Tutorial.java
@@ -0,0 +1,60 @@
+package com.bezkoder.spring.datajpa.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tutorials")
+public class Tutorial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "patient_name")
+    private String patient_name;
+ 
+    @Column(name = "birth_date")
+    private String birth_date;
+
+    @Column(name = "phone_number")
+    private String phone_number;
+ 
+    public Tutorial() {
+
+    }
+
+    public Tutorial(String patient_name, String birth_date, String phone_number) {
+        this.patient_name = patient_name;
+        this.birth_date = birth_date;
+        this.phone_number = phone_number;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getPatientName() {
+        return patient_name;
+    }
+
+    public void setPatientName(String patient_name) {
+        this.patient_name = patient_name;
+    }
+
+    public String getBirthDate() {
+        return birth_date;
+    }
+
+    public void setBirthDate(String birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public String getPhoneNumber() {
+        return phone_number;
+    }
+
+    public void setPhoneNumber(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+}
