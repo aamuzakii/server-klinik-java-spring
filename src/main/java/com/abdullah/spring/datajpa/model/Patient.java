e3c4057f3f71e742b1571e90b39822a021235276
aamuzakii <aamuzakii@gmail.com>
2022-05-30 08:01:52 +0700

    accept incoming to solve conflict

--- src/main/java/com/abdullah/spring/datajpa/model/Patient.java
+++ src/main/java/com/abdullah/spring/datajpa/model/Patient.java
@@ -6,42 +6,27 @@ import javax.persistence.*;
 @Table(name = "users")
 public class Patient {
 
-<<<<<<< HEAD:src/main/java/com/bezkoder/spring/datajpa/model/Tutorial.java
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
-=======
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id")
     private long id;
->>>>>>> 5ff4d5e9daaf711cd52ce752f6a8c7df94bc0143:src/main/java/com/abdullah/spring/datajpa/model/Patient.java
 
 	@Column(name = "title")
 	private String title;
 
-<<<<<<< HEAD:src/main/java/com/bezkoder/spring/datajpa/model/Tutorial.java
-	@Column(name = "description")
-	private String description;
-=======
     @Column(name = "phone_number")
     private String phone_number;
  
     public Patient() {
->>>>>>> 5ff4d5e9daaf711cd52ce752f6a8c7df94bc0143:src/main/java/com/abdullah/spring/datajpa/model/Patient.java
 
 	@Column(name = "published")
 	private boolean published;
 
-<<<<<<< HEAD:src/main/java/com/bezkoder/spring/datajpa/model/Tutorial.java
-	public Tutorial() {
-=======
     public Patient(String patient_name, String birth_date, String phone_number) {
         this.patient_name = patient_name;
         this.birth_date = birth_date;
         this.phone_number = phone_number;
     }
->>>>>>> 5ff4d5e9daaf711cd52ce752f6a8c7df94bc0143:src/main/java/com/abdullah/spring/datajpa/model/Patient.java
 
 	}
 
